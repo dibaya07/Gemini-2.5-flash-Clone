9efd1db9563092fb8bf4ce599b16a77ed28cbc85
dibaya07 <dibyajyotibaruah987@gmail.com>
2025-08-30 12:14:52 +0530

    error fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AuthForm from "./components/authForm";
+import AuthForm from "./components/AuthForm";
 import Home from "./components/Home";
 
-
+// /components/authForm
 function App() {
 
   const router = createBrowserRouter([
